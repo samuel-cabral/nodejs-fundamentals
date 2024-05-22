b9597fbde3212838b88c99e791fe7d611e30822b
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-05-22 15:50:19 -0300

    persisting database

--- src/database.js
+++ src/database.js
@@ -1,5 +1,26 @@
+import fs from 'node:fs/promises';
+
+const databasePath = new URL('../db.json', import.meta.url);
+
 export class Database {
   #database = {};
+
+  constructor() {
+    this.#load();
+  }
+
+  async #load() {
+    try {
+      const data = await fs.readFile(databasePath);
+      this.#database = JSON.parse(data);
+    } catch (error) {
+      this.#persist();
+    }
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
 
   select(table) {
     const data = this.#database[table] ?? [];
@@ -13,6 +34,8 @@ export class Database {
     } else {
       this.#database[table] = [data];
     }
+
+    this.#persist();
 
     return data;
   }
