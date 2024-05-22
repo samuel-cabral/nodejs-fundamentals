358280ff745f245e66599deb57173fbfd59796dd
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-05-22 20:16:31 -0300

    separating routes of the application

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from 'node:http'
-import { randomUUID } from 'node:crypto';
+
 import { json } from './middlewares/json.js';
 import { Database } from './database.js';
+import { routes } from './routes.js';
 
 const database = new Database();
 
@@ -10,26 +11,15 @@ const server = http.createServer(async (request, response) => {
 
   await json(request, response)
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
+  const route = routes.find(route => (
+    route.method === method && route.path === url
+  ))
 
-    return response
-      .end(JSON.stringify(users))
+  if (route) {
+    return route.handler(request, response)
   }
 
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = request.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    }
-
-    database.insert('users', user)
-    
-    return response.writeHead(201).end()
-  }
+  console.log(route)
 
   return response.writeHead(404).end()
 })
